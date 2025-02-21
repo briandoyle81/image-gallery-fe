2fb2430849ba28a5c55451f1ecb0313c28d23596
Brian Doyle <briandoyle81@gmail.com>
2025-02-21 12:19:07 -0500

    Try script to surface error

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,8 @@ import Header from './components/Header';
 export default function Home() {
   return (
     <div className="min-h-screen bg-gray-100">
+      <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
+      <script>eruda.init();</script>
       <Header />
       <main className="p-8 sm:p-10">
         <div className="max-w-[1200px] mx-auto">
