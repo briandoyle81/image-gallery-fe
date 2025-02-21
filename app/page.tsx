5d024c5a2e72aa3e8ec10e32bae4c978118fe404
Brian Doyle <briandoyle81@gmail.com>
2025-02-21 12:26:05 -0500

    Install eruda

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,25 @@
+declare global {
+  interface Window {
+    eruda: any;
+  }
+}
+
 'use client';
+
+import { useEffect } from "react";
+import eruda from "eruda";
 
 import Content from './components/Content';
 import Header from './components/Header';
 
 export default function Home() {
+    useEffect(() => {
+    if (typeof window !== "undefined" && !window.eruda) {
+      eruda.init();
+    }
+  }, []);
   return (
     <div className="min-h-screen bg-gray-100">
-      <script src="https://cdn.jsdelivr.net/npm/eruda"></script>
-      <script>eruda.init();</script>
       <Header />
       <main className="p-8 sm:p-10">
         <div className="max-w-[1200px] mx-auto">
